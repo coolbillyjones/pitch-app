37190f34bc4207df0023c53e71663899fbc69d88
Joseph Gonzalez <josephg5002@gmail.com>
2025-01-25 14:41:38 -0500

    AI Running

--- src/index.js
+++ src/index.js
@@ -10,6 +10,22 @@
 
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+		const res = await fetch(
+			"https://github.com/Azure-Samples/cognitive-services-speech-sdk/raw/master/samples/cpp/windows/console/samples/enrollment_audio_katie.wav"
+		)
+		const blob = await res.arrayBuffer()
+
+		const input = {
+			audio: [... new Uint8Array(blob)],
+		}
+
+		const response = await env.AI.run(
+			"@cf/openai/whisper", 
+			input
+		)
+
+		
+
+		return Response.json({ input: { audio: [] }, response});
 	},
-};
+}
